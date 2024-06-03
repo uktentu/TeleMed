7c5e525ffae58a93735644c518e786e8813a4854
Uday Kiran Tentu <uktentu@gmail.com>
2024-06-03 11:33:35 +0530

    modified

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'firfirfas@gmail.com' # this email will be used to send emails
-EMAIL_HOST_PASSWORD = 'UDAY.2244' # host email password required
+EMAIL_HOST_PASSWORD = 'UDAY@2244' # host email password required
 # now sign in with your host gmail account in your browser
 # open following link and turn it ON
 # https://myaccount.google.com/lesssecureapps
